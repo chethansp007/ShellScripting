c14d648f04152b4e4c86d9296ad13cc804583538
SIA Dev <sqmobile@singaporeair.com.sg>
2019-01-25 13:34:08 +0800

    Added the script file

---
+++ learn copy.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+MY_MESSAGE="Hello World"
+echo $MY_MESSAGE
+
+
+x="hello"
+# expr $x + 1
+
+
+MY_MESSAGE="Hello World1"
+MY_SHORT_MESSAGE=hi
+MY_NUMBER=1
+MY_PI=3.142
+MY_OTHER_PI="3.142"
+MY_MIXED=123abc
+
+echo $MY_MESSAGE
+
+
+echo What is your name?
+read MY_NAME
+echo "Hello $MY_NAME - hope you're well."
+echo "Cool $MY_NAME"
